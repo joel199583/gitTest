c43766eb2224bff0784a18dc79a07effc8de41b5
Joel Xu <xuzhewei199583@gmail.com>
2020-09-26 14:57:35 +0800

    1st version

---
+++ gitTest.java
@@ -0,0 +1,24 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class HomeWork03_02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int guessNum = (int)(Math.random()*100);
+		
+		while(true){
+			int inputNum = sc.nextInt();
+			if(inputNum>guessNum) {
+				System.out.println("錯了，在小一點 !");
+			}else if(inputNum<guessNum) {
+				System.out.println("錯了，在大一點 !");
+			}else{
+				System.out.println("恭喜你答對了!!!");
+				break;
+			}
+		}
+	}
+}
